0cfe061318977a22e0c5bc159cc0ded1887cd59c
Kevin Watters <kevinwatters@gmail.com>
2017-10-17 10:19:22 -0400

    Prettier

--- js/modelviewer.js
+++ js/modelviewer.js
@@ -1,287 +1,272 @@
-var text = window.displayText || "Foobar";
+"use strict";
+
+const text = window.displayText || "Foobar";
 var MODEL_URL = window.modelUrl || "/.downloaded/cartDog.obj";
 
-var THREE = window.THREE || {};
-var textGeo;
-var textMesh1, textMesh2;
+const THREE = window.THREE || {};
+let textGeo;
+let textMesh1, textMesh2;
 
 function loadFont() {
-    var loader = new THREE.FontLoader();
-    loader.load( '/fonts/' + fontName + '_' + fontWeight + '.typeface.json', function(response) {
-        font = response;
-        refreshText();
-    });
+  const loader = new THREE.FontLoader();
+  loader.load(
+    "/fonts/" + fontName + "_" + fontWeight + ".typeface.json",
+    function(response) {
+      font = response;
+      refreshText();
+    }
+  );
 }
 
 function refreshText() {
-    textGroup.remove(textMesh1);
-    if (mirror) textGroup.remove( textMesh2 );
-    if (!text) return;
-    createText();
+  textGroup.remove(textMesh1);
+  if (mirror) textGroup.remove(textMesh2);
+  if (!text) return;
+  createText();
 }
 
-
 function createText() {
-    textGeo = new THREE.TextBufferGeometry( text, {
-        font: font,
-        size: size,
-        height: height,
-        curveSegments: curveSegments,
-        bevelThickness: bevelThickness,
-        bevelSize: bevelSize,
-        bevelEnabled: bevelEnabled,
-        material: 0,
-        extrudeMaterial: 1
-    });
+  textGeo = new THREE.TextBufferGeometry(text, {
+    font: font,
+    size: size,
+    height: height,
+    curveSegments: curveSegments,
+    bevelThickness: bevelThickness,
+    bevelSize: bevelSize,
+    bevelEnabled: bevelEnabled,
+    material: 0,
+    extrudeMaterial: 1
+  });
 
-    textGeo.computeBoundingBox();
-    textGeo.computeVertexNormals();
+  textGeo.computeBoundingBox();
+  textGeo.computeVertexNormals();
 
-    // "fix" side normals by removing z-component of normals for side faces
-    // (this doesn't work well for beveled geometry as then we lose nice curvature around z-axis)
+  // "fix" side normals by removing z-component of normals for side faces
+  // (this doesn't work well for beveled geometry as then we lose nice curvature around z-axis)
 
-    if (!bevelEnabled) {
-        const triangleAreaHeuristics = 0.1 * ( height * size );
-        for ( var i = 0; i < textGeo.faces.length; i ++ ) {
-            const face = textGeo.faces[i];
-            if (face.materialIndex === 1) {
-
-                for (var j = 0; j < face.vertexNormals.length; j++) {
-                    face.vertexNormals[ j ].z = 0;
-                    face.vertexNormals[ j ].normalize();
-                }
-
-                var va = textGeo.vertices[face.a];
-                var vb = textGeo.vertices[face.b];
-                var vc = textGeo.vertices[face.c];
-
-                var s = THREE.GeometryUtils.triangleArea( va, vb, vc );
-
-                if (s > triangleAreaHeuristics) {
-                    for (var k = 0; k < face.vertexNormals.length; k++)
-                        face.vertexNormals[ k ].copy( face.normal );
-                }
-
-            }
-
+  if (!bevelEnabled) {
+    const triangleAreaHeuristics = 0.1 * (height * size);
+    for (var i = 0; i < textGeo.faces.length; i++) {
+      const face = textGeo.faces[i];
+      if (face.materialIndex === 1) {
+        for (var j = 0; j < face.vertexNormals.length; j++) {
+          face.vertexNormals[j].z = 0;
+          face.vertexNormals[j].normalize();
         }
 
+        var va = textGeo.vertices[face.a];
+        var vb = textGeo.vertices[face.b];
+        var vc = textGeo.vertices[face.c];
+
+        var s = THREE.GeometryUtils.triangleArea(va, vb, vc);
+
+        if (s > triangleAreaHeuristics) {
+          for (var k = 0; k < face.vertexNormals.length; k++)
+            face.vertexNormals[k].copy(face.normal);
+        }
+      }
     }
+  }
 
-    var centerOffset = -0.5 * ( textGeo.boundingBox.max.x - textGeo.boundingBox.min.x );
+  const centerOffset =
+    -0.5 * (textGeo.boundingBox.max.x - textGeo.boundingBox.min.x);
 
-    textMesh1 = new THREE.Mesh( textGeo, materials );
+  textMesh1 = new THREE.Mesh(textGeo, materials);
 
-    textMesh1.position.x = centerOffset;
-    textMesh1.position.y = hover;
-    textMesh1.position.z = 0;
+  textMesh1.position.x = centerOffset;
+  textMesh1.position.y = hover;
+  textMesh1.position.z = 0;
 
-    textMesh1.rotation.x = 0;
-    textMesh1.rotation.y = Math.PI * 2;
+  textMesh1.rotation.x = 0;
+  textMesh1.rotation.y = Math.PI * 2;
 
-    textGroup.add( textMesh1 );
+  textGroup.add(textMesh1);
 
-    if ( mirror ) {
-        textMesh2 = new THREE.Mesh( textGeo, materials );
-        textMesh2.position.set(centerOffset, -hover, height);
-        textMesh2.rotation.x = Math.PI;
-        textMesh2.rotation.y = Math.PI * 2;
-        textGroup.add( textMesh2 );
-    }
-
+  if (mirror) {
+    textMesh2 = new THREE.Mesh(textGeo, materials);
+    textMesh2.position.set(centerOffset, -hover, height);
+    textMesh2.rotation.x = Math.PI;
+    textMesh2.rotation.y = Math.PI * 2;
+    textGroup.add(textMesh2);
+  }
 }
 
 const F = 3;
-const w = (400)*F;
-const h = (400)*F;
+const w = 400 * F;
+const h = 400 * F;
 
-var scene = new THREE.Scene();
-var camera;
+const scene = new THREE.Scene();
+let camera;
 
 const skyColor = new THREE.Color();
 const groundColor = new THREE.Color();
-skyColor.setHSL( Math.random(), 1 - (Math.random() * .2), 0.5 );
-groundColor.setHSL( Math.random(), 1 - (Math.random() * .2), 0.5 );
+skyColor.setHSL(Math.random(), 1 - Math.random() * 0.2, 0.5);
+groundColor.setHSL(Math.random(), 1 - Math.random() * 0.2, 0.5);
 
-var light = new THREE.HemisphereLight(skyColor, groundColor, .7 );
-scene.add( light );
+const light = new THREE.HemisphereLight(skyColor, groundColor, 0.7);
+scene.add(light);
 
-var intensity = 0.3;
-var directionalLight = new THREE.DirectionalLight( 0xffffff, intensity );
-directionalLight.position.set( 3, 1, 3 );
-directionalLight.rotation.set(.3, .4, 0);
-scene.add( directionalLight );
+const intensity = 0.3;
+const directionalLight = new THREE.DirectionalLight(0xffffff, intensity);
+directionalLight.position.set(3, 1, 3);
+directionalLight.rotation.set(0.3, 0.4, 0);
+scene.add(directionalLight);
 
 var height = 20,
-    size = 70,
-    hover = 30,
+  size = 70,
+  hover = 30,
+  curveSegments = 4,
+  bevelThickness = 2,
+  bevelSize = 1.5,
+  //bevelSegments = 3,
+  bevelEnabled = true,
+  font = undefined,
+  fontName = "optimer", // helvetiker, optimer, gentilis, droid sans, droid serif
+  fontWeight = "bold"; // normal bold
 
-    curveSegments = 4,
-
-    bevelThickness = 2,
-    bevelSize = 1.5,
-    //bevelSegments = 3,
-    bevelEnabled = true,
-
-    font = undefined,
-
-    fontName = "optimer", // helvetiker, optimer, gentilis, droid sans, droid serif
-    fontWeight = "bold"; // normal bold
-
-var materials = [
-    new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } ), // front
-    new THREE.MeshPhongMaterial( { color: 0xffffff } ) // side
+const materials = [
+  new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true }), // front
+  new THREE.MeshPhongMaterial({ color: 0xffffff }) // side
 ];
 
-var textGroup = new THREE.Group();
+const textGroup = new THREE.Group();
 textGroup.position.y = 0;
 scene.add(textGroup);
 loadFont();
 
-var mirror = false;
+const mirror = false;
 
-var fontMap = {
-    "helvetiker": 0,
-    "optimer": 1,
-    "gentilis": 2,
-    "droid/droid_sans": 3,
-    "droid/droid_serif": 4
+const fontMap = {
+  helvetiker: 0,
+  optimer: 1,
+  gentilis: 2,
+  "droid/droid_sans": 3,
+  "droid/droid_serif": 4
 };
 
-var weightMap = {
-    "regular": 0,
-    "bold": 1
+const weightMap = {
+  regular: 0,
+  bold: 1
 };
 
-var reverseFontMap = [];
-var reverseWeightMap = [];
+const reverseFontMap = [];
+const reverseWeightMap = [];
 
-(function() {
-    for ( var i in fontMap ) reverseFontMap[ fontMap[i] ] = i;
-})();
-{
-    for ( var i in weightMap ) reverseWeightMap[ weightMap[i] ] = i;
-}
+for (const i in fontMap) reverseFontMap[fontMap[i]] = i;
+for (const i in weightMap) reverseWeightMap[weightMap[i]] = i;
 
 function measureSize(obj) {
-    return new THREE.Box3().setFromObject(obj).getSize();
+  return new THREE.Box3().setFromObject(obj).getSize();
 }
 
-function fitCameraToObj(camera, obj)
-{
-    // fit camera to object
-    var size = measureSize(obj);
-    var height = size.y;
-    var dist = height / (2 * Math.tan(camera.fov * Math.PI / 360));
-    var pos = obj.position;
+function fitCameraToObj(camera, obj) {
+  // fit camera to object
+  const size = measureSize(obj);
+  const height = Math.max(size.y, size.x) * .9;
+  const dist = height / (2 * Math.tan(camera.fov * Math.PI / 360));
+  const pos = obj.position;
 
-    // fudge factor so the object doesn't take up the whole view
-    camera.position.set(pos.x, pos.y, dist * 1.0); 
-    camera.lookAt(pos);
+  // fudge factor so the object doesn't take up the whole view
+  camera.position.set(pos.x, pos.y, dist * 1.0);
+  camera.lookAt(pos);
 
-    return size;
+  return size;
 }
-
 
 function getCenterPoint(mesh) {
-    var geometry = mesh.geometry;
-    geometry.computeBoundingBox();   
-    var center = geometry.boundingBox.getCenter();
-    mesh.localToWorld( center );
-    return center;
+  const geometry = mesh.geometry;
+  geometry.computeBoundingBox();
+  const center = geometry.boundingBox.getCenter();
+  mesh.localToWorld(center);
+  return center;
 }
 
 function getObject3DCenter(obj3d) {
-    var center = new THREE.Vector3();
-    var length = 0;
-    obj3d.traverse(function(o) {
-        if (o.geometry)
-        {
-            center.add(getCenterPoint(o));
-            length++;
-        }
-    });
-    center.divideScalar(length);
-    return center;
+  const center = new THREE.Vector3();
+  let length = 0;
+  obj3d.traverse(function(o) {
+    if (o.geometry) {
+      center.add(getCenterPoint(o));
+      length++;
+    }
+  });
+  center.divideScalar(length);
+  return center;
 }
 
-var renderer;
-
+let renderer;
 
 function loadAnyModel(path, cb) {
-    var loader;
-    if (path.toLowerCase().endsWith(".obj"))
-        loader = new THREE.OBJLoader();
-    else
-        loader = new THREE.AssimpJSONLoader();
+  var loader;
+  if (path.toLowerCase().endsWith(".obj")) loader = new THREE.OBJLoader();
+  else loader = new THREE.AssimpJSONLoader();
 
-    function err(e) {
-        console.log("Loader failed", e);
-        console.dir(e);
-    }
+  function err(e) {
+    console.log("Loader failed", e);
+    console.dir(e);
+  }
 
-    loader.load(path, cb, null, err);
+  loader.load(path, cb, null, err);
 }
 
 function start(givenRenderer, rtTexture, cb) {
-    if (givenRenderer)
-        renderer = givenRenderer;
-    else
-    {
-        renderer = new THREE.WebGLRenderer({ });
-        renderer.setSize(w, h);
-        document.body.appendChild(renderer.domElement);
-        const style = renderer.domElement.style;
-        style.width = Math.floor(w/F) + "px";
-        style.height = Math.floor(h/F) + "px";
+  if (givenRenderer) renderer = givenRenderer;
+  else {
+    renderer = new THREE.WebGLRenderer({});
+    renderer.setSize(w, h);
+    document.body.appendChild(renderer.domElement);
+    const style = renderer.domElement.style;
+    style.width = Math.floor(w / F) + "px";
+    style.height = Math.floor(h / F) + "px";
+  }
+
+  var aspect = 1;
+  camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
+
+  renderer.setClearColor(new THREE.Color(0, 0, 0), 1);
+  function render() {
+    if (!window.isHeadless) requestAnimationFrame(render);
+    renderer.clear();
+    renderer.render(scene, camera, rtTexture, true);
+    if (cb) {
+      cb();
+      cb = undefined;
     }
+  }
 
-    var aspect = 1;
-    camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
-
-    renderer.setClearColor(new THREE.Color(0, 0, 0), 1);
-    function render() {
-        if (!window.isHeadless) requestAnimationFrame(render);
-        renderer.clear();
-        renderer.render(scene, camera, rtTexture, true);
-        if (cb)
-        {
-            cb();
-            cb = undefined;
-        }
-    }
-
-    const loadAllResources = () => new Promise((resolve, reject) => {
-        THREE.DefaultLoadingManager.onLoad = resolve;
-        THREE.DefaultLoadingManager.onError = reject;
+  const loadAllResources = () =>
+    new Promise((resolve, reject) => {
+      THREE.DefaultLoadingManager.onLoad = resolve;
+      THREE.DefaultLoadingManager.onError = reject;
     });
 
-    loadAnyModel(MODEL_URL, function(obj) {
-        scene.add(obj);
+  loadAnyModel(MODEL_URL, function(obj) {
+    scene.add(obj);
 
-        const textSize = measureSize(textGroup);
-        const objCenter = getObject3DCenter(obj);
-        obj.position.sub(objCenter);
-        const objSize = measureSize(obj);
+    const textSize = measureSize(textGroup);
+    const objCenter = getObject3DCenter(obj);
+    obj.position.sub(objCenter);
+    const objSize = measureSize(obj);
 
-        const factor = textSize.x  / objSize.x;
-        console.log(factor);
-        textGroup.scale.set(1/factor, 1/factor, 1/factor);
+    const sign = Math.random() < 5 ? -1 : 1;
+    obj.rotation.y = sign * (Math.random() / 2 + 0.3);
+    obj.position.x += sign * objSize.x / 4;
 
-        fitCameraToObj(camera, scene);
+    const factor = textSize.x / objSize.x;
+    console.log(factor);
+    textGroup.scale.set(1 / factor, 1 / factor, 1 / factor);
+    textGroup.position.y = objSize.y / 10 * 2;
 
-        if (!window.isHeadless)
-        {
-            var controls = new THREE.OrbitControls(camera, renderer.domElement);
-            controls.addEventListener("change", render); // remove when using animation loop
-        }
+    fitCameraToObj(camera, scene);
 
-        loadAllResources().then(render);
-    });
+    if (!window.isHeadless) {
+      var controls = new THREE.OrbitControls(camera, renderer.domElement);
+      controls.addEventListener("change", render); // remove when using animation loop
+    }
+
+    loadAllResources().then(render);
+  });
 }
 
-if (!window.isHeadless)
-    start();
-else
-    module.exports.start = start;
-
+if (!window.isHeadless) start();
+else module.exports.start = start;
--- src/headless.js
+++ src/headless.js
@@ -1,215 +1,234 @@
 "use strict";
 
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-const fs = require('fs');
+const fs = require("fs");
 const path = require("path");
-const PNG = require('pngjs').PNG;
+const PNG = require("pngjs").PNG;
 const sharp = require("sharp");
 const assert = require("assert");
-const THREE = require('three');
+const THREE = require("three");
 
 function loadImage(filename, cb) {
-    const image = sharp(filename);
-    let metadata;
-    image.metadata()
-        .then((md) => {
-            metadata = md;
-            return image.raw().toBuffer();
-        })
-        .then((buf) => {
-            const {width, height, channels} = metadata;
-            const expectedLength = width * height * channels;
-            assert.equal(buf.length, expectedLength);
-            cb(null, {width, height, channels, data: buf});
-        })
-        .catch((err) => cb(err));
+  const image = sharp(filename);
+  let metadata;
+  image
+    .metadata()
+    .then(md => {
+      metadata = md;
+      return image.raw().toBuffer();
+    })
+    .then(buf => {
+      const { width, height, channels } = metadata;
+      const expectedLength = width * height * channels;
+      assert.equal(buf.length, expectedLength);
+      cb(null, { width, height, channels, data: buf });
+    })
+    .catch(err => cb(err));
 }
 
 //const oldLoad = THREE.TextureLoader.prototype.load;
-THREE.TextureLoader.prototype.load = function load(url, onLoad, onProgress, onError) {
-    if (url === undefined) url = '';
-    url = this.path + url;
-    const scope = this;
-    scope.manager.itemStart(url);
-    const texture = new THREE.DataTexture();//data, width, height, format);
-    texture.image = {data:null, width:0, height:0};
-    loadImage(url, (err, image) => {
-        try {
-            if (err)
-            {
-                if (onError) onError(err);
-                else console.error(err.stack);
-                scope.manager.itemEnd( url );
-                scope.manager.itemError( url );
-                return;
-            }
+THREE.TextureLoader.prototype.load = function load(
+  url,
+  onLoad,
+  onProgress,
+  onError
+) {
+  if (url === undefined) url = "";
+  url = this.path + url;
+  const scope = this;
+  scope.manager.itemStart(url);
+  const texture = new THREE.DataTexture(); //data, width, height, format);
+  texture.image = { data: null, width: 0, height: 0 };
+  loadImage(url, (err, image) => {
+    try {
+      if (err) {
+        if (onError) onError(err);
+        else console.error(err.stack);
+        scope.manager.itemEnd(url);
+        scope.manager.itemError(url);
+        return;
+      }
 
-            const {data, width, height, channels} = image;
-            texture.image = {data, width, height};
-            let format = THREE.RGBAFormat;
-            if (channels === 3) format = THREE.RGBFormat;
-            else if (channels === 4) format = THREE.RGBAFormat;
-            else {
-                const msg = "unknown number of channels: " + channels;
-                console.error(msg);
-                if (onError) onError(msg);
-                scope.manager.itemEnd( url );
-                scope.manager.itemError( url );
-                return;
-            }
-            texture.format = format;
-            texture.minFilter = THREE.LinearFilter;
-            texture.needsUpdate = true;
-            if (onLoad) onLoad(texture);
-            scope.manager.itemEnd( url );
-        } catch(err) {
-            console.error(err.stack);
-            scope.manager.itemEnd( url );
-            scope.manager.itemError( url );
-            throw err;
-        }
-    });
-    return texture;
-    //return oldLoad.call(this, url, onLoad, onProgress, onError);
+      const { data, width, height, channels } = image;
+      texture.image = { data, width, height };
+      let format = THREE.RGBAFormat;
+      if (channels === 3) format = THREE.RGBFormat;
+      else if (channels === 4) format = THREE.RGBAFormat;
+      else {
+        const msg = "unknown number of channels: " + channels;
+        console.error(msg);
+        if (onError) onError(msg);
+        scope.manager.itemEnd(url);
+        scope.manager.itemError(url);
+        return;
+      }
+      texture.format = format;
+      texture.minFilter = THREE.LinearFilter;
+      texture.needsUpdate = true;
+      if (onLoad) onLoad(texture);
+      scope.manager.itemEnd(url);
+    } catch (err) {
+      console.error(err.stack);
+      scope.manager.itemEnd(url);
+      scope.manager.itemError(url);
+      throw err;
+    }
+  });
+  return texture;
+  //return oldLoad.call(this, url, onLoad, onProgress, onError);
 };
 
 // hack in FileLoader for nodejs
-THREE.FileLoader.prototype.load = function load(url, onLoad, onProgress, onError) {
-    if ( url === undefined ) url = '';
-    if ( this.path !== undefined ) url = this.path + url;
-    this.manager.itemStart(url);
-    if (url.startsWith("/"))
-        url = url.substr(1);
-    if (!fs.existsSync(url))
-    {
-        this.manager.itemEnd(url);
-        this.manager.itemError(url);
-        throw new Error(url);
-    }
-    fs.readFile(url, (err, data) => {
-        if (err) onError(err);
-        else onLoad(data.toString());
+THREE.FileLoader.prototype.load = function load(
+  url,
+  onLoad,
+  onProgress,
+  onError
+) {
+  if (url === undefined) url = "";
+  if (this.path !== undefined) url = this.path + url;
+  this.manager.itemStart(url);
+  if (url.startsWith("/")) url = url.substr(1);
+  if (!fs.existsSync(url)) {
+    this.manager.itemEnd(url);
+    this.manager.itemError(url);
+    throw new Error(url);
+  }
+  fs.readFile(url, (err, data) => {
+    if (err) onError(err);
+    else onLoad(data.toString());
 
-        this.manager.itemEnd(url);
-        if (err) this.manager.itemError(url);
-    });
+    this.manager.itemEnd(url);
+    if (err) this.manager.itemError(url);
+  });
 };
 
 global.window = {
-    addEventListener: function() {},
-    removeEventListener: function() {},
-    isHeadless: true,
+  addEventListener: function() {},
+  removeEventListener: function() {},
+  isHeadless: true,
+  THREE: THREE
 };
 global.XMLHttpRequest = XMLHttpRequest;
-
-
-global.window.THREE = THREE;
 global.THREE = THREE;
 
 require("../js/loaders/OBJLoader.js");
 require("../js/loaders/AssimpJSONLoader.js");
 
-var gl = require("headless-gl")(512, 512);
-var width = 600;
-var height = 400;
-var png = new PNG({width: width, height: height});
+const gl = require("headless-gl")(512, 512);
+const width = 600;
+const height = 400;
+const png = new PNG({width: width, height: height});
 
-var scene = new THREE.Scene();
+const scene = new THREE.Scene();
 
-var VIEW_ANGLE = 45;
+const VIEW_ANGLE = 45;
 var camera = new THREE.PerspectiveCamera(VIEW_ANGLE, width / height, 0.1, 100);
 scene.add(camera);
 
 camera.position.set(0, 2, 2);
-
 camera.lookAt(scene.position);
 
-var canvas = new Object();
-
+const canvas = new Object();
 console.assert(gl);
 
-var renderer = new THREE.WebGLRenderer({
-    antialias: true,
-    width: 0,
-    height: 0,
-    canvas: canvas,
-    context: gl
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+  width: 0,
+  height: 0,
+  canvas: canvas,
+  context: gl
 });
 renderer.clearColor = new THREE.Color(0, 0, 0, 1);
 
-var rtTexture = new THREE.WebGLRenderTarget(width, height, {
-    minFilter: THREE.LinearFilter,
-    magFilter: THREE.NearestFilter,
-    format: THREE.RGBAFormat
+const rtTexture = new THREE.WebGLRenderTarget(width, height, {
+  minFilter: THREE.LinearFilter,
+  magFilter: THREE.NearestFilter,
+  format: THREE.RGBAFormat
 });
 
 function filePathToLocalUrl(filename) {
-    let relativePath = path.relative(path.resolve(__dirname, ".."), filename);
-    relativePath = relativePath.replace("\\", "/");
-    return relativePath;
+  let relativePath = path.relative(path.resolve(__dirname, ".."), filename);
+  relativePath = relativePath.replace("\\", "/");
+  return relativePath;
 }
 
-const renderModelFile = module.exports.renderModelFile = function renderModelFile(filename, outputPath, displayText, cb) {
-    filename = path.resolve(filename);
-    if (!fs.existsSync(filename))
-        throw new Error("not found: " + filename);
+const renderModelFile = (module.exports.renderModelFile = function renderModelFile(
+  filename,
+  outputPath,
+  displayText,
+  cb
+) {
+  filename = path.resolve(filename);
+  if (!fs.existsSync(filename)) throw new Error("not found: " + filename);
 
-    const outputFile = path.resolve(outputPath);
-    const outputDir = path.dirname(outputFile);
-    if (!fs.existsSync(outputDir))
-        mkdirsSync(outputDir);
+  const outputFile = path.resolve(outputPath);
+  const outputDir = path.dirname(outputFile);
+  if (!fs.existsSync(outputDir)) mkdirsSync(outputDir);
 
-    global.window.modelUrl = filePathToLocalUrl(filename);
-    global.window.displayText = displayText;
+  global.window.modelUrl = filePathToLocalUrl(filename);
+  global.window.displayText = displayText;
 
-    require("../js/modelviewer").start(renderer, rtTexture, function() {
+  require("../js/modelviewer").start(renderer, rtTexture, function() {
+    var glLocal = renderer.getContext();
+    var pixels = new Uint8Array(4 * width * height);
 
-        var glLocal = renderer.getContext();
-        var pixels = new Uint8Array(4 * width * height);
+    glLocal.readPixels(
+      0,
+      0,
+      width,
+      height,
+      glLocal.RGBA,
+      glLocal.UNSIGNED_BYTE,
+      pixels
+    );
 
-        glLocal.readPixels(0, 0, width, height, glLocal.RGBA, glLocal.UNSIGNED_BYTE, pixels);
+    let j, l, i, n, k, ref, ref1;
 
-        var j, l, i, n, k, ref, ref1;
+    for (
+      j = l = 0, ref = height;
+      0 <= ref ? l < ref : l > ref;
+      j = 0 <= ref ? ++l : --l
+    ) {
+      for (
+        i = n = 0, ref1 = width;
+        0 <= ref1 ? n < ref1 : n > ref1;
+        i = 0 <= ref1 ? ++n : --n
+      ) {
+        k = j * width + i;
+        const r = pixels[4 * k];
+        const g = pixels[4 * k + 1];
+        const b = pixels[4 * k + 2];
+        const a = pixels[4 * k + 3];
+        const m = (height - j + 1) * width + i;
+        png.data[4 * m] = r;
+        png.data[4 * m + 1] = g;
+        png.data[4 * m + 2] = b;
+        png.data[4 * m + 3] = a;
+      }
+    }
 
-        for (j = l = 0, ref = height; 0 <= ref ? l < ref : l > ref; j = 0 <= ref ? ++l : --l) {
-            for (i = n = 0, ref1 = width; 0 <= ref1 ? n < ref1 : n > ref1; i = 0 <= ref1 ? ++n : --n) {
-                k = j * width + i;
-                var r = pixels[4 * k];
-                var g = pixels[4 * k + 1];
-                var b = pixels[4 * k + 2];
-                var a = pixels[4 * k + 3];
-                var m = (height - j + 1) * width + i;
-                png.data[4 * m] = r;
-                png.data[4 * m + 1] = g;
-                png.data[4 * m + 2] = b;
-                png.data[4 * m + 3] = a;
-            }
-        }
-
-        const stream = fs.createWriteStream(outputPath);
-        png.pack().pipe(stream);
-        stream.on('close', function() {
-            console.log("Image written: " + outputPath);
-            if (cb) cb(null, outputPath);
-        });
-
+    const stream = fs.createWriteStream(outputPath);
+    png.pack().pipe(stream);
+    stream.on("close", function() {
+      console.log("Image written: " + outputPath);
+      if (cb) cb(null, outputPath);
     });
-};
+  });
+});
 
-function mkdirsSync(targetDir)
-{
-    const fs = require('fs');
-    const path = require('path');
-    const sep = path.sep;
-    const initDir = path.isAbsolute(targetDir) ? sep : '';
-    targetDir.split(sep).reduce((parentDir, childDir) => {
-        const curDir = path.resolve(parentDir, childDir);
-        if (!fs.existsSync(curDir))
-            fs.mkdirSync(curDir);
-        return curDir;
-    }, initDir);
+function mkdirsSync(targetDir) {
+  const fs = require("fs");
+  const path = require("path");
+  const sep = path.sep;
+  const initDir = path.isAbsolute(targetDir) ? sep : "";
+  targetDir.split(sep).reduce((parentDir, childDir) => {
+    const curDir = path.resolve(parentDir, childDir);
+    if (!fs.existsSync(curDir)) fs.mkdirSync(curDir);
+    return curDir;
+  }, initDir);
 }
 
 if (require.main === module) {
-    renderModelFile(process.argv[2], process.argv[3]);
+  renderModelFile(process.argv[2], process.argv[3]);
 }
